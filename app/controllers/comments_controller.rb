bfcef2c40124a1fddf66f01090e782dcfe4f7b2f
amit-dhoundiyal <amitdhoundiyal65@gmail.com>
2018-06-12 08:46:07 +0530

    CURD API

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+	def index
+    @comment = Comment.all
+
+    render json: @comment
+  end
+ 
+ def create
+    @comment =Comment.new(comments: params[:comments],post_id: params[:post_id])
+    
+    if @comment.save
+    	
+    	render json: @comment
+      
+    else
+      render json: {error: 'process not completed'}
+    end
+  end
+ 
+  def update
+    @comment = Comment.find(params[:id])
+ 
+    if @comment.update(comments: params[:comments],post_id: params[:post_id])
+      render json: @comment
+    else
+      render json: {error: 'process not completed'}
+    end
+  end
+ 
+  def destroy
+    @comment = Comment.find(params[:id])
+    
+ 
+    if @comment.destroy
+            render json: {status: 'successful'}
+        else
+            render json: {error: 'process not completed'}
+        end
+  end
+end
