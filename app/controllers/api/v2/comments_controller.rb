a03e878d82a584a3d3cf59571aaa46b547e0852e
amit-dhoundiyal <amitdhoundiyal65@gmail.com>
2018-06-13 10:29:34 +0530

    Authentication API

--- app/controllers/api/v2/comments_controller.rb
+++ app/controllers/api/v2/comments_controller.rb
@@ -1,5 +1,40 @@
-class Api::V2::CommentssController < ApplicationController
-	
+class Api::V2::CommentsController < ApplicationController
 	def index
-	end
+    @comment = Comment.all
+
+    render json: @comment
+  end
+ 
+ def create
+    @comment =Comment.new(comments: params[:comments],post_id: params[:post_id])
+    
+    if @comment.save
+    	
+    	render json: @comment
+      
+    else
+      render json: {error: 'process not completed'}
+    end
+  end
+ 
+  def update
+    @comment = Comment.find(params[:id])
+ 
+    if @comment.update(comments: params[:comments],post_id: params[:post_id])
+      render json: @comment
+    else
+      render json: {error: 'process not completed'}
+    end
+  end
+ 
+  def destroy
+    @comment = Comment.find(params[:id])
+    
+ 
+    if @comment.destroy
+            render json: {status: 'successful'}
+        else
+            render json: {error: 'process not completed'}
+        end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   namespace 'api' do
   	namespace 'v2' do
+      resources :posts
+      resources :comments
   		resources :users ,only: :create   do
         collection do
           post 'confirm'
